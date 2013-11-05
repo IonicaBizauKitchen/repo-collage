d516f08d76f6f69e242fb3795b23970be4748915
zeke <zeke@sikelianos.com>
2013-11-04 22:26:25 -0800

    sync with firebase!

--- src/index.js
+++ src/index.js
@@ -1,18 +1,74 @@
 window.domready = require('domready');
 window.drag = require('drag');
+window.agent = require('superagent');
+window.repoCollage = new Firebase('https://repo-collage.firebaseio.com/');
+window.positions = repoCollage.child('positions');
+
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+window.app = {}
+
+app.getRepos = function (user, cb) {
+
+  app.user = user
+
+  if (localStorage.repos) {
+    console.log('use localStorage.repos')
+    return cb(JSON.parse(localStorage.repos))
+  } else {
+    agent.get("https://api.github.com/users/" + user + "/repos?per_page=1000").end(function(err, res) {
+      localStorage.repos = JSON.stringify(res.body);
+      return cb(res.body);
+    });
+  }
+}
+
+app.renderRepos = function (repos, cb) {
+
+  var $draggables = document.querySelector('.draggables');
+
+  repos.map(function(repo, index) {
+    el = document.createElement('li')
+    el.innerHTML = repo.name
+    el.id = app.user + "-" + repo.name.replace(/\./g, 'DOT')
+    $draggables.appendChild(el)
+
+    el.style.top = index * (document.querySelector('li').offsetHeight + 5);
+
+    drag(el)
+      .end(function(el) {
+        positions.child(this.el.id)
+          .set([this.pos.x, this.pos.y].join(','))
+      })
+      .bind();
+
+  });
+
+  return cb(repos);
+}
 
 domready(function() {
 
-  console.log('dom is ready');
+  app.getRepos('heroku', function(repos) {
+    app.renderRepos(repos, function() {
+      // console.log('done rendering repos')
+    });
+  })
 
-  NodeList.prototype.forEach = Array.prototype.forEach;
-  document.querySelectorAll('.draggables > li').forEach(function(element, index) {
-
-    element.style.top = index * document.querySelector('li').offsetHeight;
-
-    drag(element)
-      .end(function(el) { console.log(el); })
-      .bind();
+  repoCollage.on('value', function(snapshot) {
+    if (!snapshot.val()) return false;
+    var positions = snapshot.val().positions;
+    if (positions) {
+      Object.keys(positions).map(function(repo) {
+        console.log(repo, positions[repo])
+        var position = positions[repo].split(',');
+        var el = document.querySelector('#'+repo)
+        if (el) {
+          el.style.left = position[0]
+          el.style.top = position[1]
+        }
+      });
+    }
   });
 
 })
